7fcce28fef2030a92983782ff715731d48a78a2e
Merak1 <eddie_comes_for_you@hotmail.es>
2019-11-02 20:03:24 -0600

    Updates

---
+++ precache-manifest.e1dbd4d1f81cf3b6891d630557bddd36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "54b0f4111813bbd0c426",
+    "url": "/xkcd/static/css/main.ea02e0bb.chunk.css"
+  },
+  {
+    "revision": "54b0f4111813bbd0c426",
+    "url": "/xkcd/static/js/main.a199b185.chunk.js"
+  },
+  {
+    "revision": "e9b988e60ab488f780f7",
+    "url": "/xkcd/static/js/runtime~main.da37fd3e.js"
+  },
+  {
+    "revision": "5804d46474bb30a6b7a4",
+    "url": "/xkcd/static/css/2.0c3c0fac.chunk.css"
+  },
+  {
+    "revision": "5804d46474bb30a6b7a4",
+    "url": "/xkcd/static/js/2.4f70175d.chunk.js"
+  },
+  {
+    "revision": "c953148761f3ede02f3525e12d313cf7",
+    "url": "/xkcd/static/media/favorite.c9531487.png"
+  },
+  {
+    "revision": "4d0a857da80d83447b3a1e3c14c8dbfb",
+    "url": "/xkcd/index.html"
+  }
+];
